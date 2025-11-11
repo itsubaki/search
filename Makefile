6067d2aa6ada757cf454958d89a6694b225086f9
itsubaki <1759459+itsubaki@users.noreply.github.com>
2025-11-11 21:04:44 +0900

    Update some files

--- Makefile
+++ Makefile
@@ -14,4 +14,12 @@ elastic-init:
 
 
 opensearch:
-	docker run --rm --name opensearch-node -p 9200:9200 -p 9600:9600 -e "discovery.type=single-node" -e "OPENSEARCH_JAVA_OPTS=-Xms1g -Xmx1g" -e "OPENSEARCH_INITIAL_ADMIN_PASSWORD=xuYz3_cAXYh7" opensearchproject/opensearch:latest
+	docker run --rm --name opensearch -p 9200:9200 -p 9600:9600 -e "discovery.type=single-node" -e "OPENSEARCH_JAVA_OPTS=-Xms1g -Xmx1g" -e "OPENSEARCH_INITIAL_ADMIN_PASSWORD=xuYz3_cAXYh7" opensearchproject/opensearch:latest
+
+opensearch-init:
+	docker exec -it opensearch bin/opensearch-plugin install analysis-icu
+	docker exec -it opensearch bin/opensearch-plugin install analysis-kuromoji
+	docker restart opensearch
+
+opensearch-log:
+	docker logs -f opensearch
--- osr/client.go
+++ osr/client.go
@@ -1,1 +1,68 @@
 package osr
+
+import (
+	"context"
+	"crypto/tls"
+	"io"
+	"net/http"
+
+	"github.com/opensearch-project/opensearch-go"
+	"github.com/opensearch-project/opensearch-go/opensearchapi"
+)
+
+type Client struct {
+	osc *opensearch.Client
+}
+
+func NewClient(address []string, username, password string) (*Client, error) {
+	client, err := opensearch.NewClient(opensearch.Config{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true,
+			},
+		},
+		Addresses: address,
+		Username:  username,
+		Password:  password,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &Client{
+		osc: client,
+	}, nil
+}
+
+func (c *Client) Ping() error {
+	if _, err := c.osc.Ping(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Client) Delete(ctx context.Context, index []string) error {
+	resp, err := opensearchapi.IndicesDeleteRequest{
+		Index: index,
+	}.Do(ctx, c.osc)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	return nil
+}
+
+func (c *Client) Create(ctx context.Context, index string, body io.Reader) error {
+	resp, err := opensearchapi.IndicesCreateRequest{
+		Index: index,
+		Body:  body,
+	}.Do(ctx, c.osc)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	return nil
+}
